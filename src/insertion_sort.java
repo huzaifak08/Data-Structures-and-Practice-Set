587cee63a0f9674a204e81298533d88ee6803461
huzaifak08 <hk7928042@gmail.com>
2024-12-26 07:24:41 +0500

    Insertion Sort: explanation

--- src/insertion_sort.java
+++ src/insertion_sort.java
@@ -1,5 +1,30 @@
 
 public class insertion_sort {
+    // -1 0  1  2  3  4  5
+
+    //    12 | 54 65 07 23 09 --> Pass:1 , Key:54, Index:0
+
+    //    12 54 | 65 07 23 09 --> Pass:2 , Key:65, Index:1
+
+    //    12 54 65 | 07 23 09 --> Pass:3 , Key:07, Index:2
+    //    12 54 65 | 65 23 09 --> Pass:3 , Key:07, Index:1
+    //    12 54 54 | 65 23 09 --> Pass:3 , Key:07, Index:0
+    //    12 12 54 | 65 23 09 --> Pass:3 , Key:07, Index:0
+    //    07 12 54 | 65 23 09 --> Pass:3 , Key:07, Index:0
+
+    //    07 12 54 65 | 23 09 --> Pass:4 , Key:23, Index:3
+    //    07 12 54 65 | 65 09 --> Pass:4 , Key:23, Index:3
+    //    07 12 54 54 | 65 09 --> Pass:4 , Key:23, Index:2
+    //    07 12 54 54 | 65 09 --> Pass:4 , Key:23, Index:1
+    //    07 12 23 54 | 65 09 --> Pass:4 , Key:23, Index:-1
+
+    //    07 12 23 54 65 | 09 --> Pass:4 , Key:09, Index:4
+    //    07 12 23 54 65 | 65 --> Pass:4 , Key:09, Index:4
+    //    07 12 23 54 54 | 65 --> Pass:4 , Key:09, Index:3
+    //    07 12 23 23 54 | 65 --> Pass:4 , Key:09, Index:2
+    //    07 12 12 23 54 | 65 --> Pass:4 , Key:09, Index:1
+    //    07 09 12 23 54 | 65 --> Pass:4 , Key:09, Index:0
+
     // Insertion Sort:
     public static void insertionSort(int[] arr) {
         int key, index;
@@ -15,6 +40,7 @@ public class insertion_sort {
                 index--;
             }
 
+            // To place the key at its position
             arr[index+1] = key;
         }
     }
