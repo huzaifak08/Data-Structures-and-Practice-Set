8527f399a57aa9ef0f9daeaaf5bae4ca9fbdfaff
huzaifak08 <hk7928042@gmail.com>
2025-06-04 19:52:50 +0500

    leet code problem 27

---
+++ src/remove_dup_of_value.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class remove_dup_of_value {
+    public static int removeElement(int[] nums, int val) {
+
+        int i =0;
+        for (int index = 0; index < nums.length; index++){
+            if(nums[index] != val){
+                nums[i] = nums[index];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        int result = removeElement(nums,val);
+        System.out.println("Result: "+ result);
+    }
+}
