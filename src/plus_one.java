4c93b689ab547b562ca31401de468afb5c2c22c5
huzaifak08 <hk7928042@gmail.com>
2025-06-13 15:55:15 +0500

    plus one and sqrt ALH

---
+++ src/plus_one.java
@@ -0,0 +1,65 @@
+public class plus_one {
+    public static int[] plusOne(int[] digits) {
+        int digit = arrayToInteger(digits);
+
+        digit++;
+
+        int[] digitsArray = integerToArray(digit);
+
+        return digitsArray;
+    }
+
+    public static int arrayToInteger(int[] arr){
+        int result = 0;
+
+        for(int index =0 ; index < arr.length; index++){
+            result = result*10 +arr[index];
+        }
+        return result;
+    }
+
+    public static int[] integerToArray(int num){
+        String strNum = Integer.toString(num);
+
+        int[] array = new int[strNum.length()];
+
+        for(int index = 0; index < strNum.length(); index++){
+            // Convert to Integer:
+            char temp = strNum.charAt(index);
+
+            int value = Character.getNumericValue(temp);
+
+            array[index] = value;
+        }
+        return array;
+    }
+
+    public static int[] plusOneOpt(int[] arr){
+        for(int index = arr.length -1; index >= 0; index--){
+            System.out.println("Index: "+index+" Element: "+ arr[index]);
+            if(arr[index] < 9){
+                arr[index]++;
+                return arr;
+            }else{
+                System.out.println("Here");
+                arr[index] = 0;
+            }
+        }
+
+        // in case of [9] , [9,9] , [9,9,9]...
+        int[] newArr = new int[arr.length+1];
+        newArr[0] = 1;
+
+        return newArr;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {9,9};
+
+        int[] result = plusOneOpt(arr);
+
+        for(int index = 0; index < result.length; index++){
+            System.out.println(result[index]);
+        }
+    }
+}
---
+++ src/sqrt.java
@@ -0,0 +1,25 @@
+public class sqrt {
+    public static int mySqrt (int x){
+        if(x <= 0){
+            return 0;
+        }
+
+        if(x == 1){
+            return 1;
+        }
+
+        for(long index = 1; index <= x; index++){
+            System.out.println("Index : "+ index + " Square: " +(index*index));
+            if((index*index) > x){
+                return (int) index-1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int result = mySqrt(1871180207);
+        System.out.println(result);
+    }
+}
